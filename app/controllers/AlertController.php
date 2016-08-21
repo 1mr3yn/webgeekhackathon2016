a9dd9440075b84a2f9136e00f83e8a265cb033b1
Ryan Bayona <ryanbayona@gmail.com>
2016-08-21 12:56:35 +0800

    Use chika to send user notification

--- app/controllers/AlertController.php
+++ app/controllers/AlertController.php
@@ -79,7 +79,34 @@ class AlertController extends \BaseController {
 	 */
 	public function update($id)
 	{
-		//
+		 $arr_post_body = array(
+        "message_type" => "SEND",
+        "mobile_number" => "639155851517",
+        "shortcode" => "29290682",
+        "message_id" =>time(),
+        "message" => urlencode("Help is on its way!"),
+        "client_id" => "497fbac8d4714608c1ba4e97dcced68611be3007192dced0bb4f054528f51e76",
+        "secret_key" => "37c6f2bc07ebfb4d0ae5e4ede30f6eb037a84d216454328fef92a47b10dd786a"
+    );
+
+    $query_string = "";
+    foreach($arr_post_body as $key => $frow)
+    {
+        $query_string .= '&'.$key.'='.$frow;
+    }
+
+    $URL = "https://post.chikka.com/smsapi/request";
+
+    $curl_handler = curl_init();
+    curl_setopt($curl_handler, CURLOPT_URL, $URL);
+    curl_setopt($curl_handler, CURLOPT_POST, count($arr_post_body));
+    curl_setopt($curl_handler, CURLOPT_POSTFIELDS, $query_string);
+    curl_setopt($curl_handler, CURLOPT_RETURNTRANSFER, TRUE);
+    $response = curl_exec($curl_handler);
+    curl_close($curl_handler);
+		
+    exit(0);
+
 	}
 
 
